d12af0e257b7b3716d40edfdf3df2f3910112396
alex <reidsgadv@gmail.com>
2023-05-31 03:16:19 +1000

    Файловая Структура

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,30 @@
 import React from "react";
-
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// Components
+import { Actors, MovieInformation, Movies, Navbar, Profile } from "./index";
+import "./App.css";
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        {/* CssBaseline - Global Reset */}
+        <CssBaseline />
+        <Navbar />
+        {/* Routing v6 */}
+        <main className="app__content">
+          <div className="app__toolbar" />
+          <Routes>
+            <Route path="/" element={<Movies />} />
+            <Route path="/profile/:id" element={<Profile />} />
+            <Route path="/movie/:id" element={<MovieInformation />} />
+            <Route path="/actors/:id" element={<Actors />} />
+            <Route path="*" element="Error 404" />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
